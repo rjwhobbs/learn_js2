269a90e3a96fb460cba78c2ebeac949dd63956d2
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-12-07 11:36:58 +0200

    Do you even event queue

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-b();
-console.log(a);
-
-var a = 'Howzit my chana!';
-
-function b() {
-	console.log("Called b");
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
 }
+
+function clickHandler() {
+    console.log('click event!');   
+}
+
+// listen for the click event
+document.addEventListener('click', clickHandler);
+
+
+waitThreeSeconds();
+console.log('finished execution');
