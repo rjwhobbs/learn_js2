0ecaa9337d2cc60e0dbe5357c836400a67a949a0
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-10 08:09:00 +0200

    Not the best wat to make objects

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log(libName);
+var person = new Object();
+
+person['name'] = 'Zoltan';
+person['description'] = 'The Great';
+
+var nameProp = 'name';
+
+console.log(person);
+console.log(person[nameProp]);
+
+person.address = new Object();
+person.address.home = "Mars";
+console.log(person);
+
