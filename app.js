34a887b5ae28f4bfaafa98a90a2f00bc862b8add
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-11 07:59:28 +0200

    Functions are objects

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-var person = new Object();
+function log(a) {
+	a();
+}
 
-person['name'] = 'Zoltan';
-person['description'] = 'The Great';
-
-var nameProp = 'name';
-
-console.log(person);
-console.log(person[nameProp]);
-
-person.address = new Object();
-person.address.home = "Mars";
-console.log(person);
+log(function() {
+	 console.log('hi');
+ });
 
