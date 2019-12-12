114886f57bcd03b1ccb75c65e8f823e7c48476a0
Roger HOBBS <rhobbs@student.wethinkcode.co.za>
2019-12-12 07:49:17 +0200

    I never thought this would be this crazy, oh and btw this

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-function log(a) {
-	a();
+function a() {
+	console.log(this);
+	this.newVar = "SPAGETTI";
 }
 
-log(function() {
-	 console.log('hi');
- });
+a();
+console.log(newVar);
+
+let c = {
+	prop: this, //in node this is an empty object but in the browser this is the window object
+	athing: 1,
+	func: function() {
+		this.athing = 432;
+		c.athing = 7987923864986234;
+		console.log(this);
+		let b = function(something) {
+			this.athing = something;
+		}
+		b(100);
+	}
+};
+
+c.func();
+a();
+
 
