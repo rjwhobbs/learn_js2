ff5144fa91c87e5c43ed27fdb99906b1aa407ad6
Roger John William Hobbs <rhobbs@student.wethinkcode.co.za>
2019-12-12 17:56:03 +0200

    default values on args

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-function a() {
-	console.log(this);
-	this.newVar = "SPAGETTI";
+// function a() {
+// 	console.log(this);
+// 	this.newVar = "SPAGETTI";
+// }
+
+// a();
+// console.log(newVar);
+
+// let c = {
+// 	prop: this, //in node this is an empty object but in the browser this is the window object
+// 	athing: 1,
+// 	func: function() {
+// 		this.athing = 432;
+// 		c.athing = 7987923864986234;
+// 		console.log(this);
+// 		let b = function(something) {
+// 			this.athing = something;
+// 		}
+// 		b(100);
+// 	}
+// };
+
+// c.func();
+// a();
+
+function test(a, b, c = 1) {
+	console.log(a);
+	console.log(b);
+	console.log(c);
 }
 
-a();
-console.log(newVar);
-
-let c = {
-	prop: this, //in node this is an empty object but in the browser this is the window object
-	athing: 1,
-	func: function() {
-		this.athing = 432;
-		c.athing = 7987923864986234;
-		console.log(this);
-		let b = function(something) {
-			this.athing = something;
-		}
-		b(100);
-	}
-};
-
-c.func();
-a();
+test(1, 2, 3);
 
 
