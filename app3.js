e43fae6d5494e1b994a0578caa7f171c6eb1d3ae
rjwhobbs <rhobbs@student.wethinkcode.co.za>
2019-12-17 10:54:46 +0200

    Things

---
+++ app3.js
@@ -0,0 +1,20 @@
+function buildThings () {
+	var arr = [];
+	let i = 0
+	for(; i < 3; i++) { // Changing i to 'var' 3 3 3. Using let, 0 1 2
+		arr.push(		// However putting let outside of the forloop results in 3 3 3 . 
+			function () {	//Does this mean forloops have their own execution context, I guess they do, they are just functions.
+				console.log(i);
+			}
+		);
+	}
+	return arr;
+}
+
+let stuff = buildThings();
+
+stuff[0]();
+stuff[1]();
+stuff[2]();
+
+//things
